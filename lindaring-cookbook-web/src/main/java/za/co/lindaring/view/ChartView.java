021af0ff7b82bf2f5c6582b067fa582544257d5a
Lindaring <lndsimelane@gmail.com>
2019-06-08 14:04:32 +0200

    #12 Add dashboard and line graph to index.xhtml

---
+++ lindaring-cookbook-web/src/main/java/za/co/lindaring/view/ChartView.java
@@ -0,0 +1,49 @@
+package za.co.lindaring.view;
+
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+
+@ManagedBean
+public class ChartView implements Serializable {
+
+    private LineChartModel lineModel;
+
+    @PostConstruct
+    public void init() {
+        lineModel = new LineChartModel();
+        LineChartSeries s = new LineChartSeries();
+        s.setLabel("Population");
+
+        s.set(1, 5.20);
+        s.set(2, 19.63);
+        s.set(3, 59.01);
+        s.set(4, 139.76);
+        s.set(5, 300.4);
+        s.set(6, 630);
+
+        lineModel.addSeries(s);
+        lineModel.setLegendPosition("e");
+        Axis y = lineModel.getAxis(AxisType.Y);
+        y.setMin(0.5);
+        y.setMax(700);
+        y.setLabel("Millions");
+
+        Axis x = lineModel.getAxis(AxisType.X);
+        x.setMin(0);
+        x.setMax(7);
+        x.setTickInterval("1");
+        x.setLabel("Number of Years");
+
+    }
+
+    public LineChartModel getLineModel() {
+        return lineModel;
+    }
+
+}
